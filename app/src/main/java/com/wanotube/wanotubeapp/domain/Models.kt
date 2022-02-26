c2c0b229c4185c591863f331abf42e093ed0ac59
“nguyendacthienngan” <nguyendacthienngan@gmail.com>
2022-02-27 01:35:26 +0700

    Building application structure

---
+++ app/src/main/java/com/wanotube/wanotubeapp/domain/Models.kt
@@ -0,0 +1,26 @@
+package com.wanotube.wanotubeapp.domain
+
+import com.wanotube.wanotubeapp.util.smartTruncate
+
+/**
+ * Domain objects are plain Kotlin data classes that represent the things in our app. These are the
+ * objects that should be displayed on screen, or manipulated by the app.
+ *
+ * @see database for objects that are mapped to the database
+ * @see network for objects that parse or prepare network calls
+ */
+
+/**
+ * Videos represent a WanoTube video that can be displayed
+ */
+data class WanoTubeVideo(val title: String,
+                         val description: String,
+                         val url: String,
+                         val updated: String,
+                         val thumbnail: String) {
+    /**
+     * Short description is used for displaying truncated descriptions in the UI
+     */
+    val shortDescription: String
+        get() = description.smartTruncate(200)
+}
