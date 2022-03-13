dac3bde17cdd3508f8e6802e7b47029cb0fee0a2
nguyendacthienngan <nguyendacthienngan@gmail.com>
2022-03-13 21:41:34 +0700

    Adding Material Design bottom navigation menu

--- app/src/main/java/com/wanotube/wanotubeapp/MainActivity.kt
+++ app/src/main/java/com/wanotube/wanotubeapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.wanotube.wanotubeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
