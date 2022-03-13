b9a363acec3af078a6bb2e0c2610b40acb32703a
nguyendacthienngan <nguyendacthienngan@gmail.com>
2022-03-13 20:52:37 +0700

    Initial new project

--- app/src/main/java/com/wanotube/wanotubeapp/MainActivity.kt
+++ app/src/main/java/com/wanotube/wanotubeapp/MainActivity.kt
@@ -1,38 +1,11 @@
 package com.wanotube.wanotubeapp
 
-import HomeFragment
-import UserFragment
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import com.wanotube.wanotubeapp.databinding.ActivityMainBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val homeFragment= HomeFragment()
-        val userFragment= UserFragment()
-        
-        setCurrentFragment(homeFragment)
-        
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home->setCurrentFragment(homeFragment)
-                R.id.person->setCurrentFragment(userFragment)
-                R.id.settings->setCurrentFragment(homeFragment)
-            }
-            true
-        }
-
+        setContentView(R.layout.activity_main)
     }
-
-    private fun setCurrentFragment(fragment:Fragment)=
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment,fragment)
-            commit()
-        }
 }
