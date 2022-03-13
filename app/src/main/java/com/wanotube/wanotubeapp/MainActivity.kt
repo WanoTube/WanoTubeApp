4af43608061173baefddd135cd0753222006fc00
nguyendacthienngan <nguyendacthienngan@gmail.com>
2022-03-13 23:39:44 +0700

    Working on recycler view on HomePage

--- app/src/main/java/com/wanotube/wanotubeapp/MainActivity.kt
+++ app/src/main/java/com/wanotube/wanotubeapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.wanotube.wanotubeapp.databinding.ActivityMainBinding
+import com.wanotube.wanotubeapp.ui.home.HomeFragment
 
 
 class MainActivity : AppCompatActivity() {
