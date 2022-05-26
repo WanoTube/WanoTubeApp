ae380e40c45f524371ec894bfa621d772ac3d5fe
“nguyendacthienngan” <nguyendacthienngan@gmail.com>
2022-05-27 01:14:31 +0700

    Implement Save To Watch Later

--- app/src/main/java/com/wanotube/wanotubeapp/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/wanotube/wanotubeapp/ui/home/HomeAdapter.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.wanotube.wanotubeapp.IEventListener
 import com.wanotube.wanotubeapp.R
 import com.wanotube.wanotubeapp.domain.Account
@@ -50,9 +52,9 @@ class HomeAdapter(iEventListener: IEventListener) : RecyclerView.Adapter<HomeAda
         private var listener: IEventListener = listener
         private val titleView: TextView = itemView.findViewById(R.id.title)
         private val subtitleView: TextView = itemView.findViewById(R.id.subtitle)
-
         private val thumbnailVideoView: ImageView = itemView.findViewById(R.id.thumbnail_video)
         private val avatarView: ImageView = itemView.findViewById(R.id.avatar_user)
+        private val menuView: TextView = itemView.findViewById(R.id.video_menu)
 
         fun bind(item: Video, channels: List<Account>) {
             val context = thumbnailVideoView.context
@@ -85,6 +87,23 @@ class HomeAdapter(iEventListener: IEventListener) : RecyclerView.Adapter<HomeAda
                 intent.putExtra("NEED_TOKEN", false)
                 context.startActivity(intent)
             }
+
+            menuView.setOnClickListener {
+                val bottomSheetDialog = BottomSheetDialog(context)
+                bottomSheetDialog.apply {
+                    setContentView(R.layout.home_video_menu_dialog)
+                    findViewById<LinearLayout>(R.id.save_watch_later)?.setOnClickListener {
+                        saveToWatchLater()
+                    }
+                    findViewById<LinearLayout>(R.id.share)?.setOnClickListener {
+                    }
+                    show()
+                }
+            }
+        }
+        
+        private fun saveToWatchLater() {
+            
         }
 
         companion object {
