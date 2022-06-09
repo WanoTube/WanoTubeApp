d053115285192729adb7b635aac9c847b63d18b2
nguyendacthienngan <nguyendacthienngan@gmail.com>
2022-06-10 01:23:09 +0700

    Remove gradle.properties

---
+++ app/src/main/java/com/wanotube/wanotubeapp/carousel/CarouselAdapter.kt
@@ -0,0 +1,56 @@
+package com.wanotube.wanotubeapp.carousel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.common.reflect.Reflection.getPackageName
+import com.wanotube.wanotubeapp.R
+
+class CarouselAdapter: RecyclerView.Adapter<CarouselAdapter.ViewHolder>() {
+
+    var itemList =  listOf<String>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+    override fun getItemCount() = itemList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        var circleButton = itemView.findViewById<ImageButton>(R.id.circle_button)
+        fun bind(item: String) {
+            val context = itemView.context
+            val resID: Int = context.resources.getIdentifier(item, "drawable", context.packageName)
+
+            Glide.with(circleButton.context)
+                .load(if (resID == 0 || item == "none") {
+                    R.drawable.record_selector
+                } else {
+                    resID
+                })
+                .placeholder(R.drawable.image_placeholder)
+                .circleCrop()
+                .into(circleButton)
+        }
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.circle_button, parent, false)
+                return ViewHolder(view)
+            }
+        }
+    }
+}
