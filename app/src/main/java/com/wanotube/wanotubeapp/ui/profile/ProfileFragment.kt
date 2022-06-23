374879daf74fdf11a18363926a371289506ad0ad
nguyendacthienngan <nguyendacthienngan@gmail.com>
2022-06-23 22:32:06 +0700

    feature: Call Update Profile API

--- app/src/main/java/com/wanotube/wanotubeapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/wanotube/wanotubeapp/ui/profile/ProfileFragment.kt
@@ -20,7 +20,7 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding: FragmentProfileBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_profile, container, false
         )
